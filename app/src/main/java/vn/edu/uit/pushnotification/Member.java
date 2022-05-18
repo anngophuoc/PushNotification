d33e745c38c6d89b5ee3d0a6117df98649b601f4
HP <anngophuoc.ltk@gmail.com>
2022-05-18 10:08:16 +0700

    Push Notification

---
+++ app/src/main/java/vn/edu/uit/pushnotification/Member.java
@@ -0,0 +1,14 @@
+package vn.edu.uit.pushnotification;
+
+public class Member {
+    private String name;
+    public Member(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
